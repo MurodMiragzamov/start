bcad70669614d226c7a45e0f1add4ec0ccb28199
Hugo <blackfoxmmm@gmail.com>
2021-12-03 19:04:08 +0500

    end

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ const selectEl = (selector) => document.querySelector(selector),
   createDom = (element) => document.createElement(element),
   normalizeDate = (time) => {
     const year = new Date(time).getFullYear();
+
     const month = String(new Date(time).getMonth() + 1).padStart(2, "0");
+
     const day = String(new Date(time).getDate()).padStart(2, "0");
+
     return day + "." + month + "." + year;
   };
+
+const elP = selectEl(".p");
+
+elP.style.color = "green";
